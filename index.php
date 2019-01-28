8fe1a7d47c9e63257e95b104fb2187fbba3b95e2
jdiego <jdiego@I1-3>
2019-01-28 11:58:59 +0100

    Primer COMMIT

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo "Esto es un proyecto de prueba GIT";
+            echo "Primera linea que escribo";
+            echo "Voy a guardar el primer COMMIT";
+        ?>
+    </body>
+</html>
